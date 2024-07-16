396c08c0aaedfc4373a6fdfaef14908e608a7101
Daniil <danil.baybakov.72@gmail.com>
2024-07-17 00:03:04 +0500

    diploma - start

---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic import StrictStr, SecretStr
+
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class SiteSettings(BaseSettings):
+    site_api_key: SecretStr = os.getenv("SITE_API_KEY", None)
+    site_api_host: StrictStr = os.getenv("SITE_API_HOST", None)
